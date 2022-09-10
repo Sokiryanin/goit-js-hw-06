3f10b337d97016bf93fe7a55be4d0e31af7479d8
Your Name <eugene.sidorov@me.com>
2022-09-10 13:25:02 +0300

    add task 4

---
+++ js/task-04.js
@@ -0,0 +1,29 @@
+const counterValue = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+
+// console.log(decrementBtn);
+// console.log(incrementBtn);
+// console.log(valueEl);
+
+decrementBtn.addEventListener("click", function () {
+  counterValue.decrement();
+  //   console.log(counterValue);
+  valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener("click", function () {
+  counterValue.increment();
+  //   console.log(counterValue);
+  valueEl.textContent = counterValue.value;
+});
