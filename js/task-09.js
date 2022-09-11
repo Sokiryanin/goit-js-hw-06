37c6ecedfb1e18c00f623847483538a45d606249
Your Name <eugene.sidorov@me.com>
2022-09-11 19:01:08 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColorBtn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector(".body");
+
+changeColorBtn.addEventListener("click", changeColorClick);
+
+function changeColorClick() {
+  const currentColor = getRandomHexColor();
+  color.textContent = currentColor;
+  body.getElementsByClassName.backgroundColor = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
