8e8604402efa04e01170bde676c2b57cae7b0c84
Your Name <eugene.sidorov@me.com>
2022-09-11 18:50:43 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  //   console.log("сабмит формы");
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть всі поля");
+    return;
+  }
+  console.log(`email: ${email.value}, password:${password.value}`);
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
